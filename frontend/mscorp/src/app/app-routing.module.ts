a0e44e08e91938e4f0aa99f190d01c246020b502
Erdaut <erdauttorehan@icloud.com>
2023-04-14 14:43:46 +0600

    added welcome page and router

--- frontend/mscorp/src/app/app-routing.module.ts
+++ frontend/mscorp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: WelcomePageComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/mscorp/src/app/app.module.ts
+++ frontend/mscorp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {LoginComponent} from "./login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    WelcomePageComponent,
     LoginComponent
   ],
   imports: [
