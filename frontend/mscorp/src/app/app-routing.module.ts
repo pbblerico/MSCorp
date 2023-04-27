7ff701ce2f9dc8797472b0a89451bce708a20aa6
Erdaut <erdauttorehan@icloud.com>
2023-04-27 21:12:47 +0600

    added mnogo chego (in main side)

--- frontend/mscorp/src/app/app-routing.module.ts
+++ frontend/mscorp/src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ import {PersonalAccountComponent} from "./personal-account/personal-account.comp
 import {ProfileComponent} from "./profile/profile.component";
 import {SecurityComponent} from "./security/security.component";
 import {MainPageComponent} from "./main-page/main-page.component";
+import {HealthComponent} from "./health/health.component";
+import {EventsComponent} from "./events/events.component";
+import {HistoryComponent} from "./history/history.component";
+import {ActionsComponent} from "./actions/actions.component";
+import {CreateAccountComponent} from "./create-account/create-account.component";
 
 const routes: Routes = [
   {path: 'home', component: WelcomePageComponent},
@@ -15,11 +20,18 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'personal', component: PersonalAccountComponent},
-  {path: 'main', component: MainPageComponent},
+  {path: 'main', component: MainPageComponent, children: [
+      {path: 'health', component: HealthComponent},
+      {path: 'events', component: EventsComponent},
+      {path: 'history', component: HistoryComponent},
+      {path: 'actions', component: ActionsComponent},
+      {path: 'add-event', component: CreateAccountComponent}
+    ]},
   {path: 'personal', component: PersonalAccountComponent, children: [
       {path: 'profile', component: ProfileComponent},
       {path: 'security', component: SecurityComponent}
     ]},
+
   {path: '**', component: NotFoundComponent},
 
 ];
--- frontend/mscorp/src/app/app.module.ts
+++ frontend/mscorp/src/app/app.module.ts
@@ -4,21 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
-import {LoginComponent} from "./login/login.component";
+import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
+import { LoginComponent } from "./login/login.component";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {SignUpComponent} from "./sign-up/sign-up.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
 // import {TestComponent} from "./test/test.component";
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from "@angular/forms";
-import {ReactiveFormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { PersonalAccountComponent } from './personal-account/personal-account.component';
 import { UserNavbarComponent } from './user-navbar/user-navbar.component';
-import {MainPageComponent} from "./main-page/main-page.component";
-import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { MainPageComponent } from "./main-page/main-page.component";
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { HealthComponent } from './health/health.component';
+import { EventsComponent } from './events/events.component';
+import { HistoryComponent } from './history/history.component';
+import { ActionsComponent } from './actions/actions.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 @NgModule({
@@ -31,6 +36,11 @@ import { far } from '@fortawesome/free-regular-svg-icons';
     PersonalAccountComponent,
     UserNavbarComponent,
     MainPageComponent,
+    HealthComponent,
+    EventsComponent,
+    HistoryComponent,
+    ActionsComponent,
+    CreateAccountComponent,
     // TestComponent,
   ],
   imports: [
